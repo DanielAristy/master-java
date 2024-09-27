0f681f5bb66c95449d8428538bfa265da93f753d
Daniel Aristizabal <danielaristy22@gmail.com>
2024-09-26 19:15:52 -0500

    Ejemplos del uso de string con algunos metodos importantes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         ejemploInicial();
-
+        metodosString();
     }
 
     private static void ejemploInicial() {
@@ -19,5 +22,45 @@ public class Main {
         }
 
         System.out.println(nombre);
+    }
+
+    private static void metodosString() {
+        System.out.println("Hola \"mundo\"");
+
+        String str1 = "Daniel Gomez";
+        String str2 = new String("Daniel Gomez");
+
+        // Por refercencia
+        System.out.println("Son el mismo objeto? " + (str1 == str2));
+        // length()
+        System.out.println("La cantidad de caracteres del objeto str1 = " + (str1.length()));
+        /*Se compara por valor
+        * Metodo length()*/
+        System.out.println("Son el mismo objeto? " + (str1.equals(str2)));
+        /* Ignora mayusculas o minusculas
+        * equalsIgnoreCase() */
+        System.out.println("Son el mismo objeto? " + (str1.equalsIgnoreCase(str2)));
+        /*.compareTo
+        * compara contra la cadena del argumento:
+        * 1. Un valor negativo si la cadena es anterior a b
+        * 2. Cero (0) si la cadena es igual a b
+        * 3. Un valor positivo si la cadena es posterior a b
+        * */
+        System.out.println("" + str1.compareTo(str2));
+        /*trim()
+        * Crea un nuevo objeto eliminando el espacio al inicio o al final de la cadena*/
+        String parrafo = "  Hola como estan todos  ";
+        System.out.println("Eliminar espacios de " + parrafo + " -> "+ parrafo.trim());
+        /*char[] toCharArray
+        * Converte una cadena en un arreglo de caracteres*/
+        String mensaje = "aniversario";
+        char[] caracteres = mensaje.toCharArray();
+        System.out.println("mensaje.toCharArray() = " + Arrays.toString(caracteres));
+        String alphabet = "Trabajalenguas";
+        System.out.println("alphabet.replaceAll(\"a\", \".\") = " + alphabet.replaceAll("a", ".")); // Retorna una nueva instancia imutabilidad
+        System.out.println("alphabet.indexOf('a') = " + alphabet.indexOf('a')); // Primera ocurrencia
+        System.out.println("alphabet.lastIndexOf('a') = " + alphabet.lastIndexOf('a')); // Ultima ocurrencia
+        System.out.println("alphabet.indexOf('a') = " + alphabet.lastIndexOf('z')); // Si no existe ningun caracter retorna -1
+
     }
 }
