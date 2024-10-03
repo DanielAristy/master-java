e797d44e6be95ef2dec8fcb69dac95d2b6a24102
Daniel Aristizabal <danielaristy22@gmail.com>
2024-10-03 13:55:15 -0500

    Se realizan ejemplos para los operadoress aritmeticos

---
+++ src/main/java/org/example/OperadoresAritmeticos.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int i = 5, j = 4, suma = i + j, resta = i - j, multi = i * j, div = i / j;
+        float div2 = (float) i / j;
+        System.out.println("suma = " + suma);
+        System.out.println("resta = " + resta);
+        System.out.println("multiplicacion = " + multi);
+        System.out.println("division = " + div);
+        System.out.println("division 2 = " + div2);
+
+    }
+}
