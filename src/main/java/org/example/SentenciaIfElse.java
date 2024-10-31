1313ab3c874bcc76fcae8140eecfcbc447fe02cf
Daniel Aristizabal <danielaristy22@gmail.com>
2024-10-31 10:56:20 -0500

    Manejo de sentencias if else

---
+++ src/main/java/org/example/SentenciaIfElse.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class SentenciaIfElse {
+    public static void main(String[] args) {
+        boolean expresion = false;
+        if (expresion){
+            System.out.println("Ejecutando el if");
+        } else {
+            System.out.println("Ejecutando el else");
+        }
+    }
+}
